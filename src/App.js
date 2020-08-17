5d969dabb4ced43e5cdbc23ed1842cb799b7b422
Marcos Santiago <hello@marcossantiago.com>
2020-08-17 14:55:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Button, Alert, Breadcrumb, Card} from 'react-bootstrap'
+import {Button, Alert, Breadcrumb, Card, Form, Row, Col, InputGroup} from 'react-bootstrap'
 
 
 
@@ -10,7 +10,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Card className="mb-3" style={{color:"#000", marginBottom:"#000"}}>
+       <form>
+           <Form.Row>
+               <Form.Group as={Col} controlId="formGridEmail">
+                   <InputGroup className="mb-2">
+                       <InputGroup.Prepend>
+                           <InputGroup.Text>@</InputGroup.Text>
+                       </InputGroup.Prepend>
+                   <Form.Control type="email" placeholder="Enter email" />
+                   </InputGroup>
+                   </Form.Group>
+
+               <Form.Group as={Col} controlId="formGridPassword">
+
+                   <Form.Control type="password" placeholder="Password" />
+               </Form.Group>
+               <Col xs="auto">
+               <Button type="submit" className="mb-2">Login</Button>
+               </Col>
+           </Form.Row>
+
+
+
+
+           </form>
+          <Card className="mb-3" style={{color:"#000", marginBottom:"#000"}}>
           <Card.Img src={"https://picsum.photos/200/100"} />
           <Card.Body>
             <Card.Title>
